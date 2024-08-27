71b8913226a6a336c070c22b11b37ba9d1d882e2
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-08-27 13:10:29 +0900

    24.08.27 3

--- chap01-literal-and-varialble-lecture-source/src/main/java/com/ohgiraffers/section05/typecasting/Application4.java
+++ chap01-literal-and-varialble-lecture-source/src/main/java/com/ohgiraffers/section05/typecasting/Application4.java
@@ -16,6 +16,28 @@ public class Application4 {
         double height = 175.5;
         int floorHeight = (int) height;
 
+
+        /*- 실습문제 3
+
+            국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤,
+            총점과 평균을 정수 형태로 출력하세요.
+
+            -- 출력 예시 --
+            초 201
+            평균 : 67
+        */
+
+        double kor = 80.5;
+        double mth = 50.6;
+        double eng = 70.8;
+
+        int total = (int) (kor + mth + eng);
+        int avr = (int) ((kor + mth + eng)/3);
+
+        System.out.println("총점 : " + total);
+        System.out.println("평균 : " + avr);
+
+
     }
 
 }
