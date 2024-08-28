588bf3dce701d91452fa79108e96676f7068899a
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-08-28 17:32:06 +0900

    24.08.28 2

---
+++ chap03-meethod-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/PR1.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section01.method;
+
+public class PR1 {
+
+    public static void main(String[] args) {
+
+        PR1 hi = new PR1();
+        hi.JEM();
+        hi.Hello();
+        hi.LSH();
+        hi.Hello();
+        hi.LSH();
+        hi.Hello();
+        hi.YEJ();
+        hi.Hello();
+
+    }
+
+    public void JEM() {
+        System.out.print("은미");
+    }
+
+    public void MJH() {
+        System.out.print("정현");
+    }
+
+    public void LSH() {
+        System.out.print("승현");
+    }
+
+    public void YEJ() {
+        System.out.print("이정");
+    }
+
+    public void Hello() {
+        System.out.println("님 안녕하세요?");
+    }
+
+
+
+}
---
+++ chap03-meethod-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/PR2.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.method;
+
+public class PR2 {
+
+    public static void main(String[] args) {
+
+        // hour에는 현재 시간이 들어있습니다. 삼항 연산자를 이용해 hour가 12보다 작으면`
+        // "오전", 그렇지 않으면 "오후"라는 값을 ampm이 가지도록 8행을 수정하세요`
+        // 시간은 24 시간 단위(오후 1시일 경우 13시)로 나옵니다.).`
+        //변수 = hour;
+
+
+
+
+    }
+
+}
