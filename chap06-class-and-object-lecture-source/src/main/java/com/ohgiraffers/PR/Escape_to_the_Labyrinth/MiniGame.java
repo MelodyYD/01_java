9231c91b282a373e247202081153ec1f46d35e4e
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-09-10 11:34:57 +0900

    24.09.10 1

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/PR/Escape_to_the_Labyrinth/MiniGame.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/PR/Escape_to_the_Labyrinth/MiniGame.java
@@ -29,7 +29,7 @@ public class MiniGame {
             }
             switch (userRps) {
                 case 1:
-                    if ()
+//                    if ()
             }
         }
     }
