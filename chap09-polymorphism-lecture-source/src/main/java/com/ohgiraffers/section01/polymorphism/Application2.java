f1bb9a03cb04e6e1dc723e689b174543543484d6
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-09-11 18:22:03 +0900

    24.09.11 5

--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Application2.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Application2.java
@@ -5,8 +5,25 @@ public class Application2 {
 
     public static void main(String[] args) {
         //24.09.11 13:55
-        
+        Animal[] animal = new Animal[5];
+        animal[0] = new Dog();
+        animal[1] = new Cat();
+        animal[2] = new Dog();
+        animal[3] = new Cat();
+        animal[4] = new Dog();
 
+        for(int i = 0; i < animal.length; i++) {
+            animal[i].cry();
+        }
+
+        for(int i = 0; i < animal.length; i++) {
+            // 동물들아 너희가 개면 물고 냥이면 골골거려라
+            if (animal[i] instanceof Dog) {
+                ((Dog) animal[i]).bite();
+            } else if (animal[i] instanceof Cat) {
+                ((Cat) animal[i]).Grrrr();
+            }
+        }
 
     }
 
