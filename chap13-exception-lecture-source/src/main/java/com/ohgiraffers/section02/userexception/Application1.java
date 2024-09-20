0b912c4af17caed7535af09c690c076738dd41f3
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-09-20 18:26:31 +0900

    24.09.20 3

--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application1.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application1.java
@@ -1,5 +1,9 @@
 package com.ohgiraffers.section02.userexception;
 
+import com.ohgiraffers.section02.userexception.exception.MoneyNagativeException;
+import com.ohgiraffers.section02.userexception.exception.NotEnoughMoneyException;
+import com.ohgiraffers.section02.userexception.exception.PriceNegativeException;
+
 public class Application1 {
 
     public static void main(String[] args) {
@@ -9,7 +13,24 @@ public class Application1 {
 
         ExceptionTest2 et = new ExceptionTest2();
 
-        et.checkEnoughMoney(3000, 5000);
+        try {
+            // 상품 가격 음수
+//            et.checkEnoughMoney(-3000, 5000);
+
+            // 가진 돈 음수
+//            et.checkEnoughMoney(3000, -5000);
+
+            // 물건의 가격이 가진 돈보다 큼
+            et.checkEnoughMoney(5000, 3000);
+
+
+        } catch (PriceNegativeException e) {
+            throw new RuntimeException(e);
+        } catch (MoneyNagativeException e) {
+            throw new RuntimeException(e);
+        } catch (NotEnoughMoneyException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/exception/MoneyNagativeException.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/exception/MoneyNagativeException.java
@@ -1,4 +1,9 @@
 package com.ohgiraffers.section02.userexception.exception;
 
-public class MoneyNagativeException {
+public class MoneyNagativeException extends NagativeException {
+
+    public MoneyNagativeException(String message) {
+        super(message);
+    }
+
 }
